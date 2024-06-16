3a10558913ecae6960b8c7d9325d9eaf257d330f
Juan C. Correa <j.correa.n@gmail.com>
2024-06-16 08:43:29 -0600

    MEX

--- MEX.R
+++ MEX.R
@@ -32,74 +32,74 @@ ggplot(Programas, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Programa)) +
   ggtitle("MEXICO")
 
 library(quanteda)
-TextsEC <- corpus(MEX$text)
-docvars(TextsEC, "Program") <- MEX$Program
-head(summary(TextsEC), 10)
+TextsMEX <- corpus(MEX$text)
+docvars(TextsMEX, "Program") <- MEX$Program
+head(summary(TextsMEX), 10)
 
-ProgramsEC <- tokens(TextsEC, 
+ProgramsMEX <- tokens(TextsMEX, 
                      remove_numbers = TRUE, 
                      remove_punct = TRUE, 
                      remove_url = TRUE, 
                      remove_symbols = TRUE) %>%  
   tokens_remove(stopwords("spanish"))
 
-s1 <- data.frame(kwic(ProgramsEC, pattern = phrase("pensamiento crítico")))
-s2 <- data.frame(kwic(ProgramsEC, pattern = phrase("solucionar problemas")))
-s3 <- data.frame(kwic(ProgramsEC, pattern = "comunicar"))
-s4 <- data.frame(kwic(ProgramsEC, pattern = "creatividad"))
-s5 <- data.frame(kwic(ProgramsEC, pattern = "paciencia"))
-s6 <- data.frame(kwic(ProgramsEC, pattern = "crear"))
-s7 <- data.frame(kwic(ProgramsEC, pattern = "liderar"))
-s8 <- data.frame(kwic(ProgramsEC, pattern = "resolver"))
-s9 <- data.frame(kwic(ProgramsEC, pattern = "comprometer"))
-s10 <- data.frame(kwic(ProgramsEC, pattern = "comprometerse"))
-s11 <- data.frame(kwic(ProgramsEC, pattern = "gestionar"))
-s12 <- data.frame(kwic(ProgramsEC, pattern = "reflexionar"))
-s13 <- data.frame(kwic(ProgramsEC, pattern = "controlar"))
-s14 <- data.frame(kwic(ProgramsEC, pattern = "ético"))
-s15 <- data.frame(kwic(ProgramsEC, pattern = "tolerar"))
-s16 <- data.frame(kwic(ProgramsEC, pattern = "argumentar"))
-s17 <- data.frame(kwic(ProgramsEC, pattern = "conflictos"))
-s18 <- data.frame(kwic(ProgramsEC, pattern = "negociar"))
-s19 <- data.frame(kwic(ProgramsEC, pattern = "comprender"))
-s20 <- data.frame(kwic(ProgramsEC, pattern = "equipos"))
-s21 <- data.frame(kwic(ProgramsEC, pattern = "planificar"))
-s22 <- data.frame(kwic(ProgramsEC, pattern = "generar"))
-s23 <- data.frame(kwic(ProgramsEC, pattern = "empatía"))
-s24 <- data.frame(kwic(ProgramsEC, pattern = "compartir"))
-s25 <- data.frame(kwic(ProgramsEC, pattern = "analizar"))
-s26 <- data.frame(kwic(ProgramsEC, pattern = "reconocer"))
-s27 <- data.frame(kwic(ProgramsEC, pattern = "orientar"))
-s28 <- data.frame(kwic(ProgramsEC, pattern = "respetar"))
-s29 <- data.frame(kwic(ProgramsEC, pattern = "motivar"))
-s30 <- data.frame(kwic(ProgramsEC, pattern = "cooperar"))
-s31 <- data.frame(kwic(ProgramsEC, pattern = "fortalecer"))
-s32 <- data.frame(kwic(ProgramsEC, pattern = "impulsar"))
-s33 <- data.frame(kwic(ProgramsEC, pattern = "acercar"))
-s34 <- data.frame(kwic(ProgramsEC, pattern = "ayudar"))
-s35 <- data.frame(kwic(ProgramsEC, pattern = "cambiar"))
-s36 <- data.frame(kwic(ProgramsEC, pattern = "apreciar"))
-s37 <- data.frame(kwic(ProgramsEC, pattern = "dirigir"))
-s38 <- data.frame(kwic(ProgramsEC, pattern = "fomentar"))
-s39 <- data.frame(kwic(ProgramsEC, pattern = "interactuar"))
-s40 <- data.frame(kwic(ProgramsEC, pattern = "identificar"))
-s41 <- data.frame(kwic(ProgramsEC, pattern = "competir"))
-s42 <- data.frame(kwic(ProgramsEC, pattern = "manifestar"))
-s43 <- data.frame(kwic(ProgramsEC, pattern = "responsable"))
-s44 <- data.frame(kwic(ProgramsEC, pattern = "evaluar"))
-s45 <- data.frame(kwic(ProgramsEC, pattern = "innovar"))
-s46 <- data.frame(kwic(ProgramsEC, pattern = "decidir"))
-s47 <- data.frame(kwic(ProgramsEC, pattern = phrase("tomar decisiones")))
-s48 <- data.frame(kwic(ProgramsEC, pattern = "flexibilidad"))
-s49 <- data.frame(kwic(ProgramsEC, pattern = "persua*"))
-s50 <- data.frame(kwic(ProgramsEC, pattern = "convencer"))
+s1 <- data.frame(kwic(ProgramsMEX, pattern = phrase("pensamiento crítico")))
+s2 <- data.frame(kwic(ProgramsMEX, pattern = phrase("solucionar problemas")))
+s3 <- data.frame(kwic(ProgramsMEX, pattern = "comunicar"))
+s4 <- data.frame(kwic(ProgramsMEX, pattern = "creatividad"))
+s5 <- data.frame(kwic(ProgramsMEX, pattern = "paciencia"))
+s6 <- data.frame(kwic(ProgramsMEX, pattern = "crear"))
+s7 <- data.frame(kwic(ProgramsMEX, pattern = "liderar"))
+s8 <- data.frame(kwic(ProgramsMEX, pattern = "resolver"))
+s9 <- data.frame(kwic(ProgramsMEX, pattern = "comprometer"))
+s10 <- data.frame(kwic(ProgramsMEX, pattern = "comprometerse"))
+s11 <- data.frame(kwic(ProgramsMEX, pattern = "gestionar"))
+s12 <- data.frame(kwic(ProgramsMEX, pattern = "reflexionar"))
+s13 <- data.frame(kwic(ProgramsMEX, pattern = "controlar"))
+s14 <- data.frame(kwic(ProgramsMEX, pattern = "ético"))
+s15 <- data.frame(kwic(ProgramsMEX, pattern = "tolerar"))
+s16 <- data.frame(kwic(ProgramsMEX, pattern = "argumentar"))
+s17 <- data.frame(kwic(ProgramsMEX, pattern = "conflictos"))
+s18 <- data.frame(kwic(ProgramsMEX, pattern = "negociar"))
+s19 <- data.frame(kwic(ProgramsMEX, pattern = "comprender"))
+s20 <- data.frame(kwic(ProgramsMEX, pattern = "equipos"))
+s21 <- data.frame(kwic(ProgramsMEX, pattern = "planificar"))
+s22 <- data.frame(kwic(ProgramsMEX, pattern = "generar"))
+s23 <- data.frame(kwic(ProgramsMEX, pattern = "empatía"))
+s24 <- data.frame(kwic(ProgramsMEX, pattern = "compartir"))
+s25 <- data.frame(kwic(ProgramsMEX, pattern = "analizar"))
+s26 <- data.frame(kwic(ProgramsMEX, pattern = "reconocer"))
+s27 <- data.frame(kwic(ProgramsMEX, pattern = "orientar"))
+s28 <- data.frame(kwic(ProgramsMEX, pattern = "respetar"))
+s29 <- data.frame(kwic(ProgramsMEX, pattern = "motivar"))
+s30 <- data.frame(kwic(ProgramsMEX, pattern = "cooperar"))
+s31 <- data.frame(kwic(ProgramsMEX, pattern = "fortalecer"))
+s32 <- data.frame(kwic(ProgramsMEX, pattern = "impulsar"))
+s33 <- data.frame(kwic(ProgramsMEX, pattern = "acercar"))
+s34 <- data.frame(kwic(ProgramsMEX, pattern = "ayudar"))
+s35 <- data.frame(kwic(ProgramsMEX, pattern = "cambiar"))
+s36 <- data.frame(kwic(ProgramsMEX, pattern = "apreciar"))
+s37 <- data.frame(kwic(ProgramsMEX, pattern = "dirigir"))
+s38 <- data.frame(kwic(ProgramsMEX, pattern = "fomentar"))
+s39 <- data.frame(kwic(ProgramsMEX, pattern = "interactuar"))
+s40 <- data.frame(kwic(ProgramsMEX, pattern = "identificar"))
+s41 <- data.frame(kwic(ProgramsMEX, pattern = "competir"))
+s42 <- data.frame(kwic(ProgramsMEX, pattern = "manifestar"))
+s43 <- data.frame(kwic(ProgramsMEX, pattern = "responsable"))
+s44 <- data.frame(kwic(ProgramsMEX, pattern = "evaluar"))
+s45 <- data.frame(kwic(ProgramsMEX, pattern = "innovar"))
+s46 <- data.frame(kwic(ProgramsMEX, pattern = "decidir"))
+s47 <- data.frame(kwic(ProgramsMEX, pattern = phrase("tomar decisiones")))
+s48 <- data.frame(kwic(ProgramsMEX, pattern = "flexibilidad"))
+s49 <- data.frame(kwic(ProgramsMEX, pattern = "persua*"))
+s50 <- data.frame(kwic(ProgramsMEX, pattern = "convencer"))
 
 df_list <- mget(paste0("s", 1:50))
 SS <- do.call(rbind, df_list)
 SS$Skill <- rownames(SS)
 SS$Skill <- gsub("\\..*", "", SS$Skill)
 
-SkillsEC <- data.frame(Country = "MEXador",
+SkillsMEX <- data.frame(Country = "Mexico",
                        SkillCode = c(paste0("s", 1:50)),
                        Skill = c("pensamiento crítico",
                                  "solucionar problemas",
@@ -152,20 +152,20 @@ SkillsEC <- data.frame(Country = "MEXador",
                                  "persua*",
                                  "convencer"))
 
-networkEC <- SS[c(1,8)]
-head(networkEC, 3)
+networkMEX <- SS[c(1,8)]
+head(networkMEX, 3)
 
 library(igraph)
-bn3 <- graph.data.frame(networkEC,directed=FALSE)
-bipartite_mapping(bn3)
-V(bn3)$type <- bipartite_mapping(bn3)$type
-V(bn3)$shape <- ifelse(V(bn3)$type, "circle", "square")
-V(bn3)$label.cex <- ifelse(V(bn3)$type, 0.5, 1)
-V(bn3)$size <- sqrt(igraph::degree(bn3))
-E(bn3)$color <- "lightgrey"
+bn4 <- graph.data.frame(networkMEX,directed=FALSE)
+bipartite_mapping(bn4)
+V(bn4)$type <- bipartite_mapping(bn4)$type
+V(bn4)$shape <- ifelse(V(bn4)$type, "circle", "square")
+V(bn4)$label.cex <- ifelse(V(bn4)$type, 0.5, 1)
+V(bn4)$size <- sqrt(igraph::degree(bn4))
+E(bn4)$color <- "lightgrey"
 
-bn3.pr <- bipartite.projection(bn3)
-Terms <- bn3.pr$proj2
+bn4.pr <- bipartite_projection(bn4)
+Terms <- bn4.pr$proj2
 
 pave <- data.frame(igraph::betweenness(Terms))
 
@@ -183,24 +183,24 @@ set.seed(915)
 
 plot(Terms, vertex.label.color = "black", vertex.label.cex = 0.8, vertex.color = node_colors, vertex.size = 15, edge.width = 0.5, edge.color = "lightgray", layout = layout_as_star, main = "")
 
-BNEC <- graph.data.frame(networkEC, directed = FALSE)
-ProgramsEC <- data.frame(Degree = igraph::degree(BNEC),
-                         Closeness = igraph::closeness(BNEC),
-                         Betweennes = igraph::betweenness(BNEC),
-                         Eigen = igraph::eigen_centrality(BNEC))
-ProgramsEC <- ProgramsEC[ -c(5:25) ]
-rownames(ProgramsEC)
-ProgramsEC$SS <- rownames(ProgramsEC)
-ProgramsEC <- ProgramsEC[order(ProgramsEC$SS), ]
-ProgramsEC <- ProgramsEC[grepl("s", ProgramsEC$SS), ]
-ProgramsEC <- ProgramsEC[1:4]
-colnames(ProgramsEC)[4] <- "Eigenvector"
+BNMEX <- graph.data.frame(networkMEX, directed = FALSE)
+ProgramsMEX <- data.frame(Degree = igraph::degree(BNMEX),
+                         Closeness = igraph::closeness(BNMEX),
+                         Betweennes = igraph::betweenness(BNMEX),
+                         Eigen = igraph::eigen_centrality(BNMEX))
+ProgramsMEX <- ProgramsMEX[ -c(5:25) ]
+rownames(ProgramsMEX)
+ProgramsMEX$SS <- rownames(ProgramsMEX)
+ProgramsMEX <- ProgramsMEX[order(ProgramsMEX$SS), ]
+ProgramsMEX <- ProgramsMEX[grepl("s", ProgramsMEX$SS), ]
+ProgramsMEX <- ProgramsMEX[1:4]
+colnames(ProgramsMEX)[4] <- "Eigenvector"
 
 
 
 library(psych)
 
-pairs.panels(ProgramsEC, 
+pairs.panels(ProgramsMEX, 
              method = "spearman", 
              hist.col = "#0A3A7E",
              density = TRUE,  
@@ -212,22 +212,22 @@ pairs.panels(ProgramsEC,
              lwd = 2,
              rug = TRUE,
              stars = TRUE, 
-             main = "MEXador")
+             main = "Mexico")
 
-IMEC <- as_incidence_matrix(BNEC, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNEC)$type)
-IMEC2 <- as.matrix(IMEC)
+IMEX <- as_biadjacency_matrix(BNMEX, names = TRUE, sparse = TRUE, types = bipartite_mapping(BNMEX)$type)
+IMEX2 <- as.matrix(IMEX)
 
-rownames(ProgramsEC)[order(ProgramsEC$Eigenvector, decreasing = TRUE)]
-selected_columns <- head(rownames(ProgramsEC)[order(ProgramsEC$Eigenvector, decreasing = TRUE)], 10)
+rownames(ProgramsMEX)[order(ProgramsMEX$Eigenvector, decreasing = TRUE)]
+selected_columns <- head(rownames(ProgramsMEX)[order(ProgramsMEX$Eigenvector, decreasing = TRUE)], 10)
 # Let's pick the most important soft skills
 # as per their eigenvector centrality
 
-current_column_names <- colnames(IMEC2)
+current_column_names <- colnames(IMEX2)
 
 # Subset the matrix by column names
-IMEC3 <- IMEC2[, selected_columns, drop = FALSE]
+IMEX3 <- IMEX2[, selected_columns, drop = FALSE]
 
-current_column_names <- colnames(IMEC3)
+current_column_names <- colnames(IMEX3)
 
 
 # Create a vector to hold the mapped skill names
@@ -236,11 +236,11 @@ mapped_skill_names <- character(length(current_column_names))
 
 for (i in seq_along(current_column_names)) {
   
-  skill_index <- match(current_column_names[i], SkillsEC$SkillCode)
+  skill_index <- match(current_column_names[i], SkillsMEX$SkillCode)
   
   # If a match is found, map the skill code to its name
   if (!is.na(skill_index)) {
-    mapped_skill_names[i] <- SkillsEC$Skill[skill_index]
+    mapped_skill_names[i] <- SkillsMEX$Skill[skill_index]
   } else {
     # Handle unmatched column names as needed
     # For example, assign a default value or leave blank
@@ -249,15 +249,15 @@ for (i in seq_along(current_column_names)) {
 }
 
 # Replace the column names of IM3 with the mapped skill names
-colnames(IMEC3) <- mapped_skill_names
-colnames(IMEC3)
+colnames(IMEX3) <- mapped_skill_names
+colnames(IMEX3)
 
 library(bipartite)
-plotweb(IMEC3, method = "normal", 
-        col.high = "#FCD116", 
-        bor.col.high = "#FCD116",
-        col.low = "#CE1127", 
-        bor.col.low = "#CE1127",
+plotweb(IMEX3, method = "normal", 
+        col.high = "#006847", 
+        bor.col.high = "#006847",
+        col.low = "#CE1125", 
+        bor.col.low = "#CE1125",
         col.interaction = "grey90",
         bor.col.interaction = "grey90",
         low.lablength = 0,
