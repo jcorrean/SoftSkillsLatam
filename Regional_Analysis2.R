adebefcc6fd051b5b114ca790a1bcd0ec57c86fb
Juan C. Correa <j.correa.n@gmail.com>
2024-06-17 11:28:00 -0600

    Update Regional_Analysis2.R

--- Regional_Analysis2.R
+++ Regional_Analysis2.R
@@ -5,3 +5,9 @@ load("VEN.RData")
 load("ECU.RData")
 
 rm(list=setdiff(ls(), c("IMCL3", "IMCO3", "IMEC3", "IM3", "IMEX3")))
+SKILLS <- data.frame(Chile = colSums(IMCL3),
+                    Colombia = colSums(IMCO3),
+                    Ecuador = colSums(IMEC3),
+                    Mexico = colSums(IMEX3), 
+                    Venezuela = colSums(IM3))
+write.csv(SKILLS, file = "MatrizTop10.csv")
