0b901c2f1c8bd58725c5626964ce703040cff780
Juan C. Correa <j.correa.n@gmail.com>
2024-06-25 12:56:37 -0600

    Update ergm.R

--- ergm.R
+++ ergm.R
@@ -1,5 +1,18 @@
+# Ejemplo de cómo simular una red aleatoria bipartita
+# con probabilidad de conexión igual a la observada para Argentina
+
+library(igraph)
+g <- sample_bipartite(43, 369, p = 0.0668)
+V(g)$shape <- ifelse(V(g)$type, "circle", "square")
+V(g)$color <- ifelse(V(g)$type, "red", "blue")
+V(g)$size <- 2
+
+# To visualize the graph
+plot(g, vertex.label = NA, main = "Random Bipartite Graph")
+
 library(ergm)
 library(network)
 pave <- as.network(t(IMEX2), matrix.type = 'incidence', bipartite = TRUE)
 pave$iel
 pave$gal
+
