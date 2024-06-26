e639384a6eb69e3f8ccb51fb428540f02cca1baf
Juan C. Correa <j.correa.n@gmail.com>
2024-06-25 18:02:45 -0600

    Update ergm.R

--- ergm.R
+++ ergm.R
@@ -2,10 +2,11 @@
 # con probabilidad de conexión entre nodos
 # igual a la observada para Argentina
 
-# Paso 1 ---- (Cargar la Red)
+# Paso 1 (Cargar la Red) ----
+
 load("ARG.RData")
 
-# Paso 2 ---- (Calcular la probabilidad de conexión entre nodos)
+# Paso 2 (Calcular la probabilidad de conexión entre nodos) ----
 # Para ello hay que contar el número de celdas en la red bipartita
 # representada en forma de matriz con columnas que representan
 # las habilidades y filas que representan los programas de 
@@ -21,9 +22,10 @@ ncol(IARG2) * nrow(IARG2)
 sum(IARG2 != 0)
 
 # Así pues, la probabilidad de conexiones es
-sum(IARG2 != 0) / (ncol(IARG2) * nrow(IARG2))
+probabilidad_empirica = sum(IARG2 != 0) / (ncol(IARG2) * nrow(IARG2))
+probabilidad_empirica
 
-# Paso 3 ---- (Caracterizar propiedades de la red)
+# Paso 3 (Caracterizar propiedades de la red) ----
 library(igraph)
 # Para ello hay que usar la red bipartita construida 
 # con igraph para calcular los siguientes estadísticos
@@ -35,9 +37,11 @@ edge_betweenness(bn6)
 mean(edge_betweenness(bn6))
 sd(edge_betweenness(bn6))
 
-# Paso 4 ---- (Generar red aleatoria con igual probabilidad de conexión que la red observada)
+# Paso 4 ----
+# Generar red aleatoria con igual probabilidad de conexión que la red observada
+
 library(igraph)
-g <- sample_bipartite(43, 369, p = 0.0668)
+g <- sample_bipartite(43, 369, p = probabilidad_empirica)
 V(g)$shape <- ifelse(V(g)$type, "circle", "square")
 V(g)$color <- ifelse(V(g)$type, "red", "blue")
 E(g)$color <- "lightgray"
@@ -47,8 +51,11 @@ V(g)$size <- 2
 plot(g, vertex.label = NA, main = "Random Bipartite Graph")
 
 edge_density(g)
-
-
+edge_density(bn6)
+mean(edge_betweenness(g))
+mean(edge_betweenness(bn6))
+sd(edge_betweenness(g))
+sd(edge_betweenness(bn6))
 
 
 
