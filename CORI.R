f4b75231b7d0fc77d2e9ac0bfd04c76df96be124
Juan C. Correa <j.correa.n@gmail.com>
2024-09-05 14:32:41 -0600

    Update CORI.R

--- CORI.R
+++ CORI.R
@@ -3,12 +3,12 @@ CORI <- readtext("Costa_Rica")
 CORI$doc_id <- gsub("\\.pdf$|\\.docx$", "", COL$doc_id)
 
 library(dplyr)
-COL <- mutate(COL, 
+CORI <- mutate(CORI, 
               Program = ifelse(
                 grepl("Doctorado en", text), "Doctorado",
                 ifelse(grepl("Maestría|Magíster en|MAGISTER EN", text), "Maestría", 
                        "Especialización")))
-Programas <- data.frame(table(COL$Program))
+Programas <- data.frame(table(CORI$Program))
 colnames(Programas)[1] <- "Programa" 
 colnames(Programas)[2] <- "Total"
 
@@ -29,77 +29,77 @@ ggplot(Programas, aes(ymax=ymax, ymin=ymin, xmax=4, xmin=3, fill=Programa)) +
   xlim(c(-1, 4)) +
   theme_void() +
   theme(legend.position = "none") + 
-  ggtitle("COLOMBIA")
+  ggtitle("COSTA RICA")
 
 library(quanteda)
-TextsCO <- corpus(COL$text)
-docvars(TextsCO, "Program") <- COL$Program
-head(summary(TextsCO), 10)
+TextsCR <- corpus(CORI$text)
+docvars(TextsCR, "Program") <- CORI$Program
+head(summary(TextsCR), 10)
 
-ProgramsCO <- tokens(TextsCO, 
+ProgramsCR <- tokens(TextsCR, 
                      remove_numbers = TRUE, 
                      remove_punct = TRUE, 
                      remove_url = TRUE, 
                      remove_symbols = TRUE) %>%  
   tokens_remove(stopwords("spanish"))
 
-s1 <- data.frame(kwic(ProgramsCO, pattern = phrase("pensamiento crítico")))
-s2 <- data.frame(kwic(ProgramsCO, pattern = phrase("solucionar problemas")))
-s3 <- data.frame(kwic(ProgramsCO, pattern = "comunicar"))
-s4 <- data.frame(kwic(ProgramsCO, pattern = "creatividad"))
-s5 <- data.frame(kwic(ProgramsCO, pattern = "paciencia"))
-s6 <- data.frame(kwic(ProgramsCO, pattern = "crear"))
-s7 <- data.frame(kwic(ProgramsCO, pattern = "liderar"))
-s8 <- data.frame(kwic(ProgramsCO, pattern = "resolver"))
-s9 <- data.frame(kwic(ProgramsCO, pattern = "comprometer"))
-s10 <- data.frame(kwic(ProgramsCO, pattern = "comprometerse"))
-s11 <- data.frame(kwic(ProgramsCO, pattern = "gestionar"))
-s12 <- data.frame(kwic(ProgramsCO, pattern = "reflexionar"))
-s13 <- data.frame(kwic(ProgramsCO, pattern = "controlar"))
-s14 <- data.frame(kwic(ProgramsCO, pattern = "ético"))
-s15 <- data.frame(kwic(ProgramsCO, pattern = "tolerar"))
-s16 <- data.frame(kwic(ProgramsCO, pattern = "argumentar"))
-s17 <- data.frame(kwic(ProgramsCO, pattern = "conflictos"))
-s18 <- data.frame(kwic(ProgramsCO, pattern = "negociar"))
-s19 <- data.frame(kwic(ProgramsCO, pattern = "comprender"))
-s20 <- data.frame(kwic(ProgramsCO, pattern = "equipos"))
-s21 <- data.frame(kwic(ProgramsCO, pattern = "planificar"))
-s22 <- data.frame(kwic(ProgramsCO, pattern = "generar"))
-s23 <- data.frame(kwic(ProgramsCO, pattern = "empatía"))
-s24 <- data.frame(kwic(ProgramsCO, pattern = "compartir"))
-s25 <- data.frame(kwic(ProgramsCO, pattern = "analizar"))
-s26 <- data.frame(kwic(ProgramsCO, pattern = "reconocer"))
-s27 <- data.frame(kwic(ProgramsCO, pattern = "orientar"))
-s28 <- data.frame(kwic(ProgramsCO, pattern = "respetar"))
-s29 <- data.frame(kwic(ProgramsCO, pattern = "motivar"))
-s30 <- data.frame(kwic(ProgramsCO, pattern = "cooperar"))
-s31 <- data.frame(kwic(ProgramsCO, pattern = "fortalecer"))
-s32 <- data.frame(kwic(ProgramsCO, pattern = "impulsar"))
-s33 <- data.frame(kwic(ProgramsCO, pattern = "acercar"))
-s34 <- data.frame(kwic(ProgramsCO, pattern = "ayudar"))
-s35 <- data.frame(kwic(ProgramsCO, pattern = "cambiar"))
-s36 <- data.frame(kwic(ProgramsCO, pattern = "apreciar"))
-s37 <- data.frame(kwic(ProgramsCO, pattern = "dirigir"))
-s38 <- data.frame(kwic(ProgramsCO, pattern = "fomentar"))
-s39 <- data.frame(kwic(ProgramsCO, pattern = "interactuar"))
-s40 <- data.frame(kwic(ProgramsCO, pattern = "identificar"))
-s41 <- data.frame(kwic(ProgramsCO, pattern = "competir"))
-s42 <- data.frame(kwic(ProgramsCO, pattern = "manifestar"))
-s43 <- data.frame(kwic(ProgramsCO, pattern = "responsable"))
-s44 <- data.frame(kwic(ProgramsCO, pattern = "evaluar"))
-s45 <- data.frame(kwic(ProgramsCO, pattern = "innovar"))
-s46 <- data.frame(kwic(ProgramsCO, pattern = "decidir"))
-s47 <- data.frame(kwic(ProgramsCO, pattern = phrase("tomar decisiones")))
-s48 <- data.frame(kwic(ProgramsCO, pattern = "flexibilidad"))
-s49 <- data.frame(kwic(ProgramsCO, pattern = "persua*"))
-s50 <- data.frame(kwic(ProgramsCO, pattern = "convencer"))
+s1 <- data.frame(kwic(ProgramsCR, pattern = phrase("pensamiento crítico")))
+s2 <- data.frame(kwic(ProgramsCR, pattern = phrase("solucionar problemas")))
+s3 <- data.frame(kwic(ProgramsCR, pattern = "comunicar"))
+s4 <- data.frame(kwic(ProgramsCR, pattern = "creatividad"))
+s5 <- data.frame(kwic(ProgramsCR, pattern = "paciencia"))
+s6 <- data.frame(kwic(ProgramsCR, pattern = "crear"))
+s7 <- data.frame(kwic(ProgramsCR, pattern = "liderar"))
+s8 <- data.frame(kwic(ProgramsCR, pattern = "resolver"))
+s9 <- data.frame(kwic(ProgramsCR, pattern = "comprometer"))
+s10 <- data.frame(kwic(ProgramsCR, pattern = "comprometerse"))
+s11 <- data.frame(kwic(ProgramsCR, pattern = "gestionar"))
+s12 <- data.frame(kwic(ProgramsCR, pattern = "reflexionar"))
+s13 <- data.frame(kwic(ProgramsCR, pattern = "controlar"))
+s14 <- data.frame(kwic(ProgramsCR, pattern = "ético"))
+s15 <- data.frame(kwic(ProgramsCR, pattern = "tolerar"))
+s16 <- data.frame(kwic(ProgramsCR, pattern = "argumentar"))
+s17 <- data.frame(kwic(ProgramsCR, pattern = "conflictos"))
+s18 <- data.frame(kwic(ProgramsCR, pattern = "negociar"))
+s19 <- data.frame(kwic(ProgramsCR, pattern = "comprender"))
+s20 <- data.frame(kwic(ProgramsCR, pattern = "equipos"))
+s21 <- data.frame(kwic(ProgramsCR, pattern = "planificar"))
+s22 <- data.frame(kwic(ProgramsCR, pattern = "generar"))
+s23 <- data.frame(kwic(ProgramsCR, pattern = "empatía"))
+s24 <- data.frame(kwic(ProgramsCR, pattern = "compartir"))
+s25 <- data.frame(kwic(ProgramsCR, pattern = "analizar"))
+s26 <- data.frame(kwic(ProgramsCR, pattern = "reconocer"))
+s27 <- data.frame(kwic(ProgramsCR, pattern = "orientar"))
+s28 <- data.frame(kwic(ProgramsCR, pattern = "respetar"))
+s29 <- data.frame(kwic(ProgramsCR, pattern = "motivar"))
+s30 <- data.frame(kwic(ProgramsCR, pattern = "cooperar"))
+s31 <- data.frame(kwic(ProgramsCR, pattern = "fortalecer"))
+s32 <- data.frame(kwic(ProgramsCR, pattern = "impulsar"))
+s33 <- data.frame(kwic(ProgramsCR, pattern = "acercar"))
+s34 <- data.frame(kwic(ProgramsCR, pattern = "ayudar"))
+s35 <- data.frame(kwic(ProgramsCR, pattern = "cambiar"))
+s36 <- data.frame(kwic(ProgramsCR, pattern = "apreciar"))
+s37 <- data.frame(kwic(ProgramsCR, pattern = "dirigir"))
+s38 <- data.frame(kwic(ProgramsCR, pattern = "fomentar"))
+s39 <- data.frame(kwic(ProgramsCR, pattern = "interactuar"))
+s40 <- data.frame(kwic(ProgramsCR, pattern = "identificar"))
+s41 <- data.frame(kwic(ProgramsCR, pattern = "competir"))
+s42 <- data.frame(kwic(ProgramsCR, pattern = "manifestar"))
+s43 <- data.frame(kwic(ProgramsCR, pattern = "responsable"))
+s44 <- data.frame(kwic(ProgramsCR, pattern = "evaluar"))
+s45 <- data.frame(kwic(ProgramsCR, pattern = "innovar"))
+s46 <- data.frame(kwic(ProgramsCR, pattern = "decidir"))
+s47 <- data.frame(kwic(ProgramsCR, pattern = phrase("tomar decisiones")))
+s48 <- data.frame(kwic(ProgramsCR, pattern = "flexibilidad"))
+s49 <- data.frame(kwic(ProgramsCR, pattern = "persua*"))
+s50 <- data.frame(kwic(ProgramsCR, pattern = "convencer"))
 
 df_list <- mget(paste0("s", 1:50))
 SS <- do.call(rbind, df_list)
 SS$Skill <- rownames(SS)
 SS$Skill <- gsub("\\..*", "", SS$Skill)
 
-SkillsCO <- data.frame(Country = "Colombia",
+SkillsCR <- data.frame(Country = "Costa Rica",
                        SkillCode = c(paste0("s", 1:50)),
                        Skill = c("pensamiento crítico",
                                  "solucionar problemas",
@@ -152,20 +152,20 @@ SkillsCO <- data.frame(Country = "Colombia",
                                  "persua*",
                                  "convencer"))
 
-networkCO <- SS[c(8,1)]
-head(networkCO, 3)
+networkCR <- SS[c(8,1)]
+head(networkCR, 3)
 
 library(igraph)
-bn1 <- graph.data.frame(networkCO,directed=FALSE)
-bipartite_mapping(bn1)
-V(bn1)$type <- bipartite_mapping(bn1)$type
-V(bn1)$shape <- ifelse(V(bn1)$type, "circle", "square")
-V(bn1)$label.cex <- ifelse(V(bn1)$type, 0.5, 1)
-V(bn1)$size <- sqrt(igraph::degree(bn1))
-E(bn1)$color <- "lightgrey"
+bn9 <- graph_from_data_frame(networkCR,directed=FALSE)
+bipartite_mapping(bn9)
+V(bn9)$type <- bipartite_mapping(bn9)$type
+V(bn9)$shape <- ifelse(V(bn9)$type, "circle", "square")
+V(bn9)$label.cex <- ifelse(V(bn9)$type, 0.5, 1)
+V(bn9)$size <- sqrt(igraph::degree(bn9))
+E(bn9)$color <- "lightgrey"
 
-bn1.pr <- bipartite.projection(bn1)
-Terms <- bn1.pr$proj2
+bn9.pr <- bipartite_projection(bn9)
+Terms <- bn9.pr$proj2
 
 pave <- data.frame(igraph::betweenness(Terms))
 
@@ -183,17 +183,17 @@ set.seed(915)
 
 plot(Terms, vertex.label.color = "black", vertex.label.cex = 0.8, vertex.color = node_colors, vertex.size = 15, edge.width = 0.5, edge.color = "lightgray", layout = layout_as_star, main = "")
 
-BNCO <- graph.data.frame(networkCO, directed = FALSE)
-ProgramsCO <- data.frame(Degree = igraph::degree(BNCO),
-                         Closeness = igraph::closeness(BNCO),
-                         Betweennes = igraph::betweenness(BNCO),
-                         Eigen = igraph::eigen_centrality(BNCO))
-ProgramsCO <- ProgramsCO[ -c(5:25) ]
-rownames(ProgramsCO)
-ProgramsCO$SS <- rownames(ProgramsCO)
-ProgramsCO <- ProgramsCO[order(ProgramsCO$SS), ]
-ProgramsCO <- ProgramsCO[grepl("s", ProgramsCO$SS), ]
-ProgramsCO <- ProgramsCO[1:4]
-colnames(ProgramsCO)[4] <- "Eigenvector"
+BNCR <- graph.data.frame(networkCR, directed = FALSE)
+ProgramsCR <- data.frame(Degree = igraph::degree(BNCR),
+                         Closeness = igraph::closeness(BNCR),
+                         Betweennes = igraph::betweenness(BNCR),
+                         Eigen = igraph::eigen_centrality(BNCR))
+ProgramsCR <- ProgramsCR[ -c(5:25) ]
+rownames(ProgramsCR)
+ProgramsCR$SS <- rownames(ProgramsCR)
+ProgramsCR <- ProgramsCR[order(ProgramsCR$SS), ]
+ProgramsCR <- ProgramsCR[grepl("s", ProgramsCR$SS), ]
+ProgramsCR <- ProgramsCR[1:4]
+colnames(ProgramsCR)[4] <- "Eigenvector"
 
 save.image("~/Documents/GitHub/SoftSkillsLatam/COL.RData")
