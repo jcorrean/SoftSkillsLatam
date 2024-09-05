dff23d37974a289cbc0977f0ba6ac1d9264dd483
Juan C. Correa <j.correa.n@gmail.com>
2024-09-05 16:13:18 -0600

    Update LatamNetworks.R

--- LatamNetworks.R
+++ LatamNetworks.R
@@ -8,8 +8,8 @@ load("ECU.RData")#bn3 Ecuador (370)
 load("MEX.RData")#bn8 Mexico (112)
 load("URU.RData")#bn7 Uruguay (114)
 load("VEN.RData")#bn2 Venezuela (119)
-load("CORI.RData")#bn9 Costa Rica ()
-rm(list=setdiff(ls(), c("bn1","bn2","bn3","bn4","bn5","bn6","bn7","bn8")))
+load("CORI.RData")#bn9 Costa Rica (59)
+rm(list=setdiff(ls(), c("bn1","bn2","bn3","bn4","bn5","bn6","bn7","bn8","bn9")))
 
 BiM1 <- as_biadjacency_matrix(bn1, types = V(bn1)$type, names = TRUE)
 BiM2 <- as_biadjacency_matrix(bn2, types = V(bn2)$type, names = TRUE)
@@ -19,6 +19,7 @@ BiM5 <- as_biadjacency_matrix(bn5, types = V(bn5)$type, names = TRUE)
 BiM6 <- as_biadjacency_matrix(bn6, types = V(bn6)$type, names = TRUE)
 BiM7 <- as_biadjacency_matrix(bn7, types = V(bn7)$type, names = TRUE)
 BiM8 <- as_biadjacency_matrix(bn8, types = V(bn8)$type, names = TRUE)
+BiM9 <- as_biadjacency_matrix(bn9, types = V(bn9)$type, names = TRUE)
 
 TotalPrograms <- ncol(BiM1) +
   ncol(BiM2) +
@@ -27,7 +28,8 @@ TotalPrograms <- ncol(BiM1) +
   ncol(BiM5) +
   ncol(BiM6) +
   ncol(BiM7) +
-  ncol(BiM8)
+  ncol(BiM8) +
+  ncol(BiM9)
 TotalPrograms
 
 red1 <- network(BiM1, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
