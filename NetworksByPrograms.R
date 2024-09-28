86ae55ada95f1e4b108f106776e5bae7fa8aee34
Juan C. Correa <j.correa.n@gmail.com>
2024-09-27 23:26:19 -0600

    Update NetworksByPrograms.R

--- NetworksByPrograms.R
+++ NetworksByPrograms.R
@@ -8,7 +8,7 @@ load("Results/Mexico.RData")
 load("Results/Uruguay.RData")
 load("Results/Venezuela.RData")
 
-rm(list=setdiff(ls(), c("ProgramsARG1", "ProgramsARG1", "ProgramsARG3",
+rm(list=setdiff(ls(), c("ProgramsARG1", "ProgramsARG2", "ProgramsARG3",
                         "ProgramsBRA1", "ProgramsBRA2", "ProgramsBRA3",
                         "ProgramsCHL1", "ProgramsCHL2", "ProgramsCHL3",
                         "ProgramsCOL1", "ProgramsCOL2", "ProgramsCOL3",
@@ -18,4 +18,13 @@ rm(list=setdiff(ls(), c("ProgramsARG1", "ProgramsARG1", "ProgramsARG3",
                         "ProgramsURU1", "ProgramsURU2", "ProgramsURU3",
                         "ProgramsVEN1", "ProgramsVEN2", "ProgramsVEN3")))
 
-
+Specializations <- do.call(rbind, list(ProgramsARG1,
+                                       ProgramsBRA1,
+                                       ProgramsCHL1,
+                                       ProgramsCOL1,
+                                       ProgramsCORI1,
+                                       ProgramsECU1,
+                                       ProgramsMEX1,
+                                       ProgramsURU1,
+                                       ProgramsVEN1))
+Masters <- 
