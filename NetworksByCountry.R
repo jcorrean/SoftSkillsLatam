075bfb1c19338e9e66275b19a0d569bfc492eba8
Juan C. Correa <j.correa.n@gmail.com>
2024-09-29 10:15:03 -0600

    Create NetworksByCountry.R

---
+++ NetworksByCountry.R
@@ -0,0 +1,37 @@
+load("Results/Argentina.RData")
+load("Results/Brazil.RData")
+load("Results/Chile.RData")
+load("Results/Colombia.RData")
+load("Results/CostaRica.RData")
+load("Results/Ecuador.RData")
+load("Results/Mexico.RData")
+load("Results/Uruguay.RData")
+load("Results/Venezuela.RData")
+
+rm(list=setdiff(ls(), c("ProgramsARG",
+                        "ProgramsBRA",
+                        "ProgramsCHL",
+                        "ProgramsCOL",
+                        "ProgramsCORI",
+                        "ProgramsECU",
+                        "ProgramsMEX",
+                        "ProgramsURU",
+                        "ProgramsVEN")))
+AllCountries <- do.call(rbind, list(ProgramsARG,
+                                   ProgramsBRA,
+                                   ProgramsCHL,
+                                   ProgramsCOL,
+                                   ProgramsCORI,
+                                   ProgramsECU,
+                                   ProgramsMEX,
+                                   ProgramsURU,
+                                   ProgramsVEN))
+library(dplyr)
+result <- AllCountries %>%
+  group_by(Partition,Country) %>%
+  summarize(
+    N = length(Node),
+    Mean = mean(Eigenvector),
+    SD = sd(Eigenvector)
+  )
+result
