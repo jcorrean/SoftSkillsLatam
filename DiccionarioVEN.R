b57748b7ad8b8a478fdf581cb2765e1e8942dbe6
Juan C. Correa <j.correa.n@gmail.com>
2024-12-28 13:47:35 -0600

    VE

--- DiccionarioVEN.R
+++ DiccionarioVEN.R
@@ -6,7 +6,7 @@ library(dplyr)
 VEN <- mutate(VEN, 
               Program = ifelse(
                 grepl("Doctorado en", text), "Doctorado",
-                ifelse(grepl("Maestría|Magíster en|MAGISTER EN", text), "Maestría", 
+                ifelse(grepl("Maestría|Magíster en|MAGISTER EN|MAESTRÍA EN", text), "Maestría", 
                        "Especialización")))
 Programas <- data.frame(table(VEN$Program))
 colnames(Programas)[1] <- "Programa" 
@@ -87,12 +87,15 @@ edges_ve <- data.frame(
   Target = EdgeListVE[, 2],
   Country = "Venezuela"
 )
-bipartite_mapping(bnVEN)
+bnVEN <- graph_from_data_frame(edges_ve, directed = TRUE)
 V(bnVEN)$type <- bipartite_mapping(bnVEN)$type
 V(bnVEN)$shape <- ifelse(V(bnVEN)$type, "circle", "square")
 V(bnVEN)$label.cex <- ifelse(V(bnVEN)$type, 0.5, 1)
 V(bnVEN)$size <- sqrt(igraph::degree(bnVEN))
 E(bnVEN)$color <- "lightgrey"
+png(filename = "f1.png", width = 10, height = 8, units = "in", res = 300)
+plot(bnVEN, vertex.label = NA, layout = layout_as_bipartite, edge.arrow.size = 0.5,edge.arrow.width = 0.5)
+dev.off()
 
 ProgramsVEN <- data.frame(Degree = igraph::degree(bnVEN),
                           Closeness = igraph::closeness(bnVEN),
@@ -113,7 +116,10 @@ ProgramsVEN$Country <- "Venezuela"
 library(psych)
 describeBy(ProgramsVEN$Eigenvector, group = ProgramsVEN$Partition, mat = TRUE, digits = 2)
 library(network)
-Venezuela <- network(Matriz, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
+verticesVEN <- nrow(Matriz) + ncol(Matriz)
+g <- network.initialize(verticesVEN, directed = TRUE, bipartite = TRUE)
+pave <- network.bipartite(Matriz, g)
+Venezuela <- network(pave, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
 Venezuela
 SizeVE <- network::network.size(Venezuela)
 DensityVE <- network::network.density(Venezuela)
@@ -125,18 +131,21 @@ set.network.attribute(Venezuela, "Country", "Venezuela")
 Venezuela
 
 bnVEN1 <- graph_from_biadjacency_matrix(t(MatrizVESPEC), directed = FALSE)
-EdgeListVE <- as_edgelist(bnVEN1)
+EdgeListVE1 <- as_edgelist(bnVEN1)
 edges_ve1 <- data.frame(
-  Source = paste0("VEN_", EdgeListVE[, 1]),
-  Target = EdgeListVE[, 2],
+  Source = paste0("VEN_", EdgeListVE1[, 1]),
+  Target = EdgeListVE1[, 2],
   Country = "Venezuela"
 )
-bipartite_mapping(bnVEN1)
+bnVEN1 <- graph_from_data_frame(edges_ve1, directed = TRUE)
 V(bnVEN1)$type <- bipartite_mapping(bnVEN1)$type
 V(bnVEN1)$shape <- ifelse(V(bnVEN1)$type, "circle", "square")
 V(bnVEN1)$label.cex <- ifelse(V(bnVEN1)$type, 0.5, 1)
 V(bnVEN1)$size <- sqrt(igraph::degree(bnVEN1))
 E(bnVEN1)$color <- "lightgrey"
+png(filename = "f2.png", width = 10, height = 8, units = "in", res = 300)
+plot(bnVEN1, vertex.label = NA, layout = layout_as_bipartite, edge.arrow.size = 0.5,edge.arrow.width = 0.5)
+dev.off()
 
 ProgramsVEN1 <- data.frame(Degree = igraph::degree(bnVEN1),
                           Closeness = igraph::closeness(bnVEN1),
@@ -158,7 +167,10 @@ ProgramsVEN1$Level <- "Specialization"
 library(psych)
 describeBy(ProgramsVEN1$Eigenvector, group = ProgramsVEN$Partition, mat = TRUE, digits = 2)
 library(network)
-Venezuela1 <- network(MatrizVESPEC, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
+verticesVENSPEC <- nrow(MatrizVESPEC) + ncol(MatrizVESPEC)
+g1 <- network.initialize(verticesVENSPEC, directed = TRUE, bipartite = TRUE)
+pave1 <- network.bipartite(MatrizVESPEC, g1)
+Venezuela1 <- network(pave1, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
 Venezuela1
 SizeVE1 <- network::network.size(Venezuela1)
 DensityVE1 <- network::network.density(Venezuela1)
@@ -167,23 +179,26 @@ set.network.attribute(Venezuela1, "Size", SizeVE1)
 set.network.attribute(Venezuela1, "Density", DensityVE1)
 set.network.attribute(Venezuela1, "Clustering", ClusteringVE1)
 set.network.attribute(Venezuela1, "Country", "Venezuela")
+set.network.attribute(Venezuela1, "Level", "Specialization")
 Venezuela1
 
 library(igraph)
 bnVEN2 <- graph_from_biadjacency_matrix(t(MatrizVEMS), directed = FALSE)
-EdgeListVE <- as_edgelist(bnVEN2)
+EdgeListVE2 <- as_edgelist(bnVEN2)
 edges_ve2 <- data.frame(
-  Source = paste0("VEN_", EdgeListVE[, 1]),
-  Target = EdgeListVE[, 2],
+  Source = paste0("VEN_", EdgeListVE2[, 1]),
+  Target = EdgeListVE2[, 2],
   Country = "Venezuela"
 )
-bipartite_mapping(bnVEN2)
+bnVEN2 <- graph_from_data_frame(edges_ve2, directed = TRUE)
 V(bnVEN2)$type <- bipartite_mapping(bnVEN2)$type
 V(bnVEN2)$shape <- ifelse(V(bnVEN2)$type, "circle", "square")
 V(bnVEN2)$label.cex <- ifelse(V(bnVEN2)$type, 0.5, 1)
 V(bnVEN2)$size <- sqrt(igraph::degree(bnVEN2))
 E(bnVEN2)$color <- "lightgrey"
-
+png(filename = "f3.png", width = 10, height = 8, units = "in", res = 300)
+plot(bnVEN2, vertex.label = NA, layout = layout_as_bipartite, edge.arrow.size = 0.5,edge.arrow.width = 0.5)
+dev.off()
 ProgramsVEN2 <- data.frame(Degree = igraph::degree(bnVEN2),
                            Closeness = igraph::closeness(bnVEN2),
                            Betweennes = igraph::betweenness(bnVEN2),
@@ -204,7 +219,10 @@ ProgramsVEN2$Level <- "Master"
 library(psych)
 describeBy(ProgramsVEN2$Eigenvector, group = ProgramsVEN2$Partition, mat = TRUE, digits = 2)
 library(network)
-Venezuela2 <- network(MatrizVEMS, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
+verticesVENMS <- nrow(MatrizVEMS) + ncol(MatrizVEMS)
+g2 <- network.initialize(verticesVENMS, directed = TRUE, bipartite = TRUE)
+pave2 <- network.bipartite(MatrizVEMS, g2)
+Venezuela2 <- network(pave2, directed = TRUE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
 Venezuela2
 SizeVE2 <- network::network.size(Venezuela2)
 DensityVE2 <- network::network.density(Venezuela2)
@@ -213,23 +231,26 @@ set.network.attribute(Venezuela2, "Size", SizeVE2)
 set.network.attribute(Venezuela2, "Density", DensityVE2)
 set.network.attribute(Venezuela2, "Clustering", ClusteringVE2)
 set.network.attribute(Venezuela2, "Country", "Venezuela")
+set.network.attribute(Venezuela2, "Level", "Master")
 Venezuela2
 
 library(igraph)
 bnVEN3 <- graph_from_biadjacency_matrix(t(MatrizVEPHD), directed = FALSE)
-EdgeListVE <- as_edgelist(bnVEN3)
+EdgeListVE3 <- as_edgelist(bnVEN3)
 edges_ve3 <- data.frame(
-  Source = paste0("VEN_", EdgeListVE[, 1]),
-  Target = EdgeListVE[, 2],
+  Source = paste0("VEN_", EdgeListVE3[, 1]),
+  Target = EdgeListVE3[, 2],
   Country = "Venezuela"
 )
-bipartite_mapping(bnVEN3)
+bnVEN3 <- graph_from_data_frame(edges_ve3, directed = TRUE)
 V(bnVEN3)$type <- bipartite_mapping(bnVEN3)$type
 V(bnVEN3)$shape <- ifelse(V(bnVEN3)$type, "circle", "square")
 V(bnVEN3)$label.cex <- ifelse(V(bnVEN3)$type, 0.5, 1)
 V(bnVEN3)$size <- sqrt(igraph::degree(bnVEN3))
 E(bnVEN3)$color <- "lightgrey"
-
+png(filename = "f4.png", width = 10, height = 8, units = "in", res = 300)
+plot(bnVEN3, vertex.label = NA, layout = layout_as_bipartite, edge.arrow.size = 0.5,edge.arrow.width = 0.5)
+dev.off()
 ProgramsVEN3 <- data.frame(Degree = igraph::degree(bnVEN3),
                            Closeness = igraph::closeness(bnVEN3),
                            Betweennes = igraph::betweenness(bnVEN3),
@@ -250,7 +271,11 @@ ProgramsVEN3$Level <- "PhD"
 library(psych)
 describeBy(ProgramsVEN3$Eigenvector, group = ProgramsVEN3$Partition, mat = TRUE, digits = 2)
 library(network)
-Venezuela3 <- network(MatrizVEPHD, directed = FALSE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
+verticesVENPHD <- nrow(MatrizVEPHD) + ncol(MatrizVEPHD)
+g3 <- network.initialize(verticesVENPHD, directed = TRUE, bipartite = TRUE)
+pave3 <- network.bipartite(MatrizVEPHD, g3)
+
+Venezuela3 <- network(pave3, directed = TRUE, hyper = FALSE, loops = FALSE, multiple = FALSE, bipartite = TRUE)
 Venezuela3
 SizeVE3 <- network::network.size(Venezuela3)
 DensityVE3 <- network::network.density(Venezuela3)
@@ -259,6 +284,7 @@ set.network.attribute(Venezuela3, "Size", SizeVE3)
 set.network.attribute(Venezuela3, "Density", DensityVE3)
 set.network.attribute(Venezuela3, "Clustering", ClusteringVE3)
 set.network.attribute(Venezuela3, "Country", "Venezuela")
+set.network.attribute(Venezuela3, "Level", "PhD")
 Venezuela3
 
 MatrizVESPEC <- as.matrix(t(VEN_Spec))
