0f16e39a2972ddc1509af75d79373aab0e54ab15
Juan C. Correa <j.correa.n@gmail.com>
2025-01-02 09:22:27 -0600

    .

--- DiccionarioBRA.R
+++ DiccionarioBRA.R
@@ -127,6 +127,7 @@ set.network.attribute(Brazil, "Size", SizeBR)
 set.network.attribute(Brazil, "Density", DensityBR)
 set.network.attribute(Brazil, "Clustering", ClusteringBR)
 set.network.attribute(Brazil, "Country", "Brazil")
+set.network.attribute(Brazil, "Level", "All")
 Brazil
 
 bnBRA1 <- graph_from_biadjacency_matrix(t(MatrizBRASPEC), directed = FALSE)
