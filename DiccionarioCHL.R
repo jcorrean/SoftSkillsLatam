750cfdcfa31c6c75a95559c909482ec326e1e4f7
Juan C. Correa <j.correa.n@gmail.com>
2025-01-09 10:34:09 -0600

    cl

--- DiccionarioCHL.R
+++ DiccionarioCHL.R
@@ -130,6 +130,7 @@ set.network.attribute(Chile, "Density", DensityCHL)
 set.network.attribute(Chile, "Clustering", ClusteringCHL)
 set.network.attribute(Chile, "Country", "Chile")
 set.network.attribute(Chile, "Level", "All")
+set.network.attribute(Chile, "OECD", TRUE)
 Chile
 
 bnCHL1 <- graph_from_biadjacency_matrix(t(MatrizCHLSPEC), directed = FALSE)
