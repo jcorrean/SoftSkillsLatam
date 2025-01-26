20b8198c62b840dc4600a2a890c3de322f0c5fde
Juan C. Correa <j.correa.n@gmail.com>
2025-01-26 16:15:14 -0600

    Create PreProcessingARG.R

---
+++ PreProcessingARG.R
@@ -0,0 +1,101 @@
+library(readtext)
+ARG <- readtext("Argentina")
+ARG$doc_id <- gsub("\\.pdf$|\\.docx$", "", ARG$doc_id)
+
+library(dplyr)
+ARG <- mutate(ARG, 
+              Program = ifelse(
+                grepl("Doctor|Doctorado en", text), "Doctorado",
+                ifelse(grepl("Maestría|Magíster en|MAGISTER EN", text), "Maestría", 
+                       "Especialización")))
+
+library(stringr)
+ARG <- ARG %>%
+  mutate(University.Code = str_extract(doc_id, "^\\d+")) 
+
+Programas <- data.frame(table(ARG$Program))
+colnames(Programas)[1] <- "Programa" 
+colnames(Programas)[2] <- "Total"
+
+library(quanteda)
+TextsARG <- corpus(ARG$text)
+docvars(TextsARG, "Program") <- ARG$Program
+docvars(TextsARG, "Country") <- "Argentina"
+head(summary(TextsARG), 10)
+ARGTexts <- data.frame(summary(TextsARG, length(TextsARG)))
+ARGSpec <- corpus_subset(TextsARG, Program == "Especialización")
+ARGMS <- corpus_subset(TextsARG, Program == "Maestría")
+ARGPhD <- corpus_subset(TextsARG, Program == "Doctorado")
+
+Dictionary <- dictionary(list(
+  active_listening = c("escucha*", "pregunta*", "cuestiona*", "entend*", "comprend*", "silencio"),
+  mathematics = c("matemática", "resolver problemas matemáticos", "cálculos", "calcular"),
+  reading_comprehension = c("lectura", "leer", "oraciones", "párrafo*", "textos", "documento*"),
+  science = c("ciencia*", "científic*", "método*", "resolver problemas", "investiga*"),
+  speaking = c("oratoria", "comunica*"),
+  writing = c("escrib*", "redact*", "escrito"),
+  active_learning = c("implicaciones", "comprende", "decisión", "futur*", "nueva información"),
+  critical_thinking = c("crítico", "pensamiento crítico", "lógic*", "razona*"),
+  learning_strategy = c("aprend*", "enseñ*", "instrucci*"),
+  monitoring = c("auto-evalu*",  "reflexi*", "desempeñ", "ejecución")
+))
+ProgramsARG <- tokens(TextsARG, 
+                      remove_numbers = TRUE, 
+                      remove_punct = TRUE, 
+                      remove_url = TRUE, 
+                      remove_symbols = TRUE) %>%  
+  tokens_remove(stopwords("spanish")) |> tokens_lookup(dictionary = Dictionary) |>
+  dfm()
+
+ARG_Spec <- tokens(ARGSpec, 
+                   remove_numbers = TRUE, 
+                   remove_punct = TRUE, 
+                   remove_url = TRUE, 
+                   remove_symbols = TRUE) %>%  
+  tokens_remove(stopwords("spanish")) |> tokens_lookup(dictionary = Dictionary) |>
+  dfm()
+
+ARG_MS <- tokens(ARGMS, 
+                 remove_numbers = TRUE, 
+                 remove_punct = TRUE, 
+                 remove_url = TRUE, 
+                 remove_symbols = TRUE) %>%  
+  tokens_remove(stopwords("spanish")) |> tokens_lookup(dictionary = Dictionary) |>
+  dfm()
+
+ARG_PhD <- tokens(ARGPhD, 
+                  remove_numbers = TRUE, 
+                  remove_punct = TRUE, 
+                  remove_url = TRUE, 
+                  remove_symbols = TRUE) %>%  
+  tokens_remove(stopwords("spanish")) |> tokens_lookup(dictionary = Dictionary) |>
+  dfm()
+
+MatrizARGSPEC <- as.matrix(ARG_Spec)
+MatrizARGMS <- as.matrix(ARG_MS)
+MatrizARGPHD <- as.matrix(ARG_PhD)
+ProgramsARG
+Matriz <- as.matrix(ProgramsARG)
+
+library(network)
+Argentina <- as.network(Matriz, matrix.type = "adjacency", directed = FALSE, bipartite = TRUE)
+SizeARG <- network::network.size(Argentina)
+DensityARG <- network::network.density(Argentina)
+ClusteringARG <- tnet::clustering_tm(t(Matriz))
+# también podría usar C4 como indicador de clustering
+# llamado como "reinforcing"
+set.network.attribute(Argentina, "Size", SizeARG)
+set.network.attribute(Argentina, "Density", DensityARG)
+set.network.attribute(Argentina, "Clustering", ClusteringARG)
+set.network.attribute(Argentina, "Country", "Argentina")
+set.network.attribute(Argentina, "Level", "All")
+set.network.attribute(Argentina, "OECD", FALSE)
+Program <- c(ARGTexts$Program, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+BrochureLength <- c(ARGTexts$Tokens, NA, NA, NA, NA, NA, NA, NA, NA, NA, NA)
+set.vertex.attribute(Argentina, "Program", Program)
+set.vertex.attribute(Argentina, "Brochure.Length", BrochureLength)
+
+get.vertex.attribute(Argentina, "vertex.names")
+get.vertex.attribute(Argentina, "Program")
+
+Argentina
