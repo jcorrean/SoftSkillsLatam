f41ad821a973e924101f6f8f506a958231829d13
Juan C. Correa <j.correa.n@gmail.com>
2025-02-08 23:54:26 -0600

    .

--- PreProcessingCHL.R
+++ PreProcessingCHL.R
@@ -1,25 +1,7 @@
 load("~/Documents/GitHub/SoftSkillsLatam/Matrices/MatrizCHL.RData")
-edges_args <- data.frame()
-for (i in 1:nrow(Matriz)) {
-  for (j in 1:ncol(Matriz)) {
-    if (Matriz[i, j] >= 1) { 
-      edges_args <- rbind(edges_args, data.frame(
-        Program = rownames(Matriz)[i],
-        Skill = colnames(Matriz)[j],
-        #Program.Skill = Matriz[i, j] > 0,
-        Frequency = Matriz[i, j]
-      ))
-    }
-  }
-}
-
-#isolated_programs <- which(rowSums(Matriz == 0) == ncol(Matriz))
-#isolated_programs
-#Matriz[rownames(Matriz) %in% isolated_programs, ]
-
-
+rm(list=setdiff(ls(), c("Matriz", "CHLTexts")))
 library(igraph)
-bnCHL <- graph_from_biadjacency_matrix(Matriz, directed = FALSE)
+bnCHL <- graph_from_biadjacency_matrix(Matriz, directed = FALSE, weighted = TRUE)
 V(bnCHL)$type <- bipartite_mapping(bnCHL)$type
 V(bnCHL)$shape <- ifelse(V(bnCHL)$type, "circle", "square")
 V(bnCHL)$color <- ifelse(V(bnCHL)$type, "red", "blue4")
@@ -27,14 +9,12 @@ V(bnCHL)$degree <- igraph::degree(bnCHL)
 V(bnCHL)$closeness <- igraph::closeness(bnCHL)
 V(bnCHL)$betweenness <- igraph::betweenness(bnCHL)
 V(bnCHL)$Eigenvector <- igraph::eigen_centrality(bnCHL)$vector
-vertices <- edges_args[edges_args$Frequency >= 1, ]
-E(bnCHL)$Frequency <- vertices$Frequency
-#E(bnCHL)$color <- "lightgrey"
-igraph::edge_attr_names(bnCHL)
-igraph::edge_attr(bnCHL)
-igraph::vertex.attributes(bnCHL)$name
+Edgelist <- data.frame(as_long_data_frame(bnCHL), stringsAsFactors = FALSE)
+Edgelist$from <- Edgelist$from_name
+Edgelist$to <- Edgelist$to_name
+Edgelist <- Edgelist[1:3]
 
-ProgramsCHL <- data.frame(vertex.names = igraph::vertex.attributes(bnCHL)$name,
+ProgramsCHL <- data.frame(vertex.names = V(bnCHL)$name,
                           is_actor = c(rep(TRUE, nrow(Matriz)), rep(FALSE, 10)),
                           node.type = c(rep("Program", nrow(Matriz)), rep("Skill", 10)),
                           Degree = V(bnCHL)$degree,
@@ -44,22 +24,27 @@ ProgramsCHL <- data.frame(vertex.names = igraph::vertex.attributes(bnCHL)$name,
                           Program = c(CHLTexts$Program, rep(NA, 10)),
                           Brochure.Length = c(CHLTexts$Tokens, rep(NA, 10)))
 
-
 library(network)
-Chile <- as.network(edges_args, 
-                        directed = FALSE, 
-                        bipartite = TRUE, 
-                        bipartite_col = "is_actor", 
-                        vertices = ProgramsCHL)
-class(Chile)
+Chile <- as.network(Matriz,
+                    loops = FALSE,
+                    directed = FALSE,
+                    bipartite = TRUE,
+                    vertices = ProgramsCHL)
 Chile
+network::list.vertex.attributes(Chile)
+delete.vertex.attribute(Chile, "na")
 network::list.edge.attributes(Chile)
-network::delete.edge.attribute(Chile, "na")
-network::list.edge.attributes(Chile)
+delete.edge.attribute(Chile, "na")
+Chile
+Chile %v% "Degree" <- ProgramsCHL$Degree
+Chile %v% "Eigenvector.centrality" <- ProgramsCHL$Eigenvector
+Chile %v% "Brochure.Length" <-  ProgramsCHL$Brochure.Length
+Chile %v% "Program" <- ProgramsCHL$Program
+Chile %e% "Freq" <- Edgelist$weight
+get.edge.attribute(Chile, "Freq")
+summary(get.edge.attribute(Chile, "Freq"))
+summary(Edgelist$weight)
 
-print(summary(network::get.edge.attribute(Chile, "Frequency")))
-
-get.edge.attribute(Chile, "Frequency")
 
 SizeARG <- network::network.size(Chile)
 DensityARG <- network::network.density(Chile)
