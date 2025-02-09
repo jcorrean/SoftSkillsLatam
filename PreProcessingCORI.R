cdd1ef077953076e43a0fdac192ff77db388be02
Juan C. Correa <j.correa.n@gmail.com>
2025-02-09 00:05:14 -0600

    Update PreProcessingCORI.R

--- PreProcessingCORI.R
+++ PreProcessingCORI.R
@@ -1,25 +1,7 @@
 load("~/Documents/GitHub/SoftSkillsLatam/Matrices/MatrizCORI.RData")
-edges_args <- data.frame()
-for (i in 1:nrow(Matriz)) {
-  for (j in 1:ncol(Matriz)) {
-    if (Matriz[i, j] >= 1) { 
-      edges_args <- rbind(edges_args, data.frame(
-        Program = rownames(Matriz)[i],
-        Skill = colnames(Matriz)[j],
-        #Program.Skill = Matriz[i, j] > 0,
-        Frequency = Matriz[i, j]
-      ))
-    }
-  }
-}
-
-#isolated_programs <- which(rowSums(Matriz == 0) == ncol(Matriz))
-#isolated_programs
-#Matriz[rownames(Matriz) %in% isolated_programs, ]
-
-
+rm(list=setdiff(ls(), c("Matriz", "CORITexts")))
 library(igraph)
-bnCR <- graph_from_biadjacency_matrix(Matriz, directed = FALSE)
+bnCR <- graph_from_biadjacency_matrix(Matriz, directed = FALSE, weighted = TRUE)
 V(bnCR)$type <- bipartite_mapping(bnCR)$type
 V(bnCR)$shape <- ifelse(V(bnCR)$type, "circle", "square")
 V(bnCR)$color <- ifelse(V(bnCR)$type, "red", "blue4")
@@ -27,14 +9,12 @@ V(bnCR)$degree <- igraph::degree(bnCR)
 V(bnCR)$closeness <- igraph::closeness(bnCR)
 V(bnCR)$betweenness <- igraph::betweenness(bnCR)
 V(bnCR)$Eigenvector <- igraph::eigen_centrality(bnCR)$vector
-vertices <- edges_args[edges_args$Frequency >= 1, ]
-E(bnCR)$Frequency <- vertices$Frequency
-#E(bnCR)$color <- "lightgrey"
-igraph::edge_attr_names(bnCR)
-igraph::edge_attr(bnCR)
-igraph::vertex.attributes(bnCR)$name
+Edgelist <- data.frame(as_long_data_frame(bnCR), stringsAsFactors = FALSE)
+Edgelist$from <- Edgelist$from_name
+Edgelist$to <- Edgelist$to_name
+Edgelist <- Edgelist[1:3]
 
-ProgramsCR <- data.frame(vertex.names = igraph::vertex.attributes(bnCR)$name,
+ProgramsCR <- data.frame(vertex.names = V(bnCR)$name,
                           is_actor = c(rep(TRUE, nrow(Matriz)), rep(FALSE, 10)),
                           node.type = c(rep("Program", nrow(Matriz)), rep("Skill", 10)),
                           Degree = V(bnCR)$degree,
@@ -44,22 +24,26 @@ ProgramsCR <- data.frame(vertex.names = igraph::vertex.attributes(bnCR)$name,
                           Program = c(CORITexts$Program, rep(NA, 10)),
                           Brochure.Length = c(CORITexts$Tokens, rep(NA, 10)))
 
-
 library(network)
-CostaRica <- as.network(edges_args, 
-                       directed = FALSE, 
-                       bipartite = TRUE, 
-                       bipartite_col = "is_actor", 
-                       vertices = ProgramsCR)
-class(CostaRica)
+CostaRica <- as.network(Matriz,
+                        loops = FALSE,
+                        directed = FALSE,
+                        bipartite = TRUE,
+                        vertices = ProgramsCR)
 CostaRica
+network::list.vertex.attributes(CostaRica)
+delete.vertex.attribute(CostaRica, "na")
 network::list.edge.attributes(CostaRica)
-network::delete.edge.attribute(CostaRica, "na")
-network::list.edge.attributes(CostaRica)
-
-print(summary(network::get.edge.attribute(CostaRica, "Frequency")))
-
-get.edge.attribute(CostaRica, "Frequency")
+delete.edge.attribute(CostaRica, "na")
+CostaRica
+CostaRica %v% "Degree" <- ProgramsCR$Degree
+CostaRica %v% "Eigenvector.centrality" <- ProgramsCR$Eigenvector
+CostaRica %v% "Brochure.Length" <-  ProgramsCR$Brochure.Length
+CostaRica %v% "Program" <- ProgramsCR$Program
+CostaRica %e% "Freq" <- Edgelist$weight
+get.edge.attribute(CostaRica, "Freq")
+summary(get.edge.attribute(CostaRica, "Freq"))
+summary(Edgelist$weight)
 
 SizeARG <- network::network.size(CostaRica)
 DensityARG <- network::network.density(CostaRica)
