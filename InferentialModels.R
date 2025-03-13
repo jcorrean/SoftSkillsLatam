8abd732ccb5be6e9e77d30766b88b414b09781c1
Juan C. Correa <j.correa.n@gmail.com>
2025-03-12 23:13:19 -0600

    Update InferentialModels.R

--- InferentialModels.R
+++ InferentialModels.R
@@ -5,16 +5,8 @@ library(ergm.multi)
 SampledNetworks <- Networks(RegionalNetworks)
 
 
-model1 <- ergm(SampledNetworks ~ edges)
-summary(model1) # AIC: 43477
-model1B <- ergm(SampledNetworks ~ edges + b1factor("Country", levels = NULL))
-summary(model1B) # AIC: 42617
-model2 <- ergm(SampledNetworks ~ edges + b1cov("Brochure.Length"))
-summary(model2) # AIC: 43157
-model3A <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = NULL))
-summary(model3A)
-model3B <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = c(8, 9, 2, 4, 1)))
-summary(model3B) # AIC: 36306
+model1 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = c(8, 9, 2, 4, 1)))
+summary(model1) # AIC: 37397
 model4 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names") + b1cov("Brochure.Length"))
 summary(model4) # AIC: 35759
 model5 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names") + b1cov("Brochure.Length") + b1factor("Country"))
