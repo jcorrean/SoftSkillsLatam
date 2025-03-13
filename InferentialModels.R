1c40bfff853f6f147d58a37b121f34cf6c87975d
Juan C. Correa <j.correa.n@gmail.com>
2025-03-12 23:55:15 -0600

    .

--- InferentialModels.R
+++ InferentialModels.R
@@ -7,7 +7,7 @@ SampledNetworks <- Networks(RegionalNetworks)
 
 model1 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = c(8, 9, 2, 4, 1)))
 summary(model1) # AIC: 37397
-model4 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names") + b1cov("Brochure.Length"))
-summary(model4) # AIC: 35759
-model5 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names") + b1cov("Brochure.Length") + b1factor("Country"))
-summary(model5) # AIC: 34821
+model2 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = c(8, 9, 2, 4, 1)) + b1cov("Brochure.Length"))
+summary(model2) # AIC: 36919
+model3 <- ergm(SampledNetworks ~ edges + b2factor("vertex.names", levels = c(8, 9, 2, 4, 1)) + b1cov("Brochure.Length") + b1factor("Country", levels = NULL))
+summary(model3) # AIC: 36004
